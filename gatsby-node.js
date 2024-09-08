becac91cb94e535841236cad93d76d6dd937964f
Rohit Nagar <nagar.2@iitj.ac.in>
2024-09-08 17:56:08 +0000

    added hooks and utils

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,7 +34,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
           '@images': path.resolve(__dirname, 'src/images'),
           '@pages': path.resolve(__dirname, 'src/pages'),
           '@styles': path.resolve(__dirname, 'src/styles'),
-        //   '@utils': path.resolve(__dirname, 'src/utils'),
+          '@utils': path.resolve(__dirname, 'src/utils'),
         },
       },
     });
